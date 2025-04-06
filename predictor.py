92acf6661c5bb2daec18ccc1e0c4648f0e8c1e77
Changfeng-Du <1606332800@qq.com>
2025-04-06 13:27:49 +0800

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -126,8 +126,8 @@ if st.button("Predict"):
     )
     
     lime_exp = lime_explainer.explain_instance(
-        data_row=input_df.values.flatten(),
-        predict_fn=model.predict_proba
+      data_row=input_df.values.flatten(),
+      predict_fn=lambda x: 1 - pmml_predict(x)  # 反转概率
     )
     
     # Display LIME explanation
